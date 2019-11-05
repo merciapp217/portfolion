5eaf30a35f71bc0bb3792acab84e8644f4e28eb7
Mell <merciapp217@icloud.com>
2019-11-05 23:55:54 +0900

    ポートフォリオサイト全体を追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <article id="index">
+      <section>
+        <div className="content">
+          <h2>PORTFOLION</h2>
+          <p>める(<a href="https://twitter.com/wellxxx_" target="_blank" rel="noopener noreferrer">@wellxxx_</a>)のポートフォリオサイトです。<br />何となく作った物を纏めて行きます。</p>
+        </div>
+        <span />
+      </section>
+    </article>
   </Layout>
 )
 
---
+++ src/pages/skils.js
@@ -0,0 +1,52 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import skilsFile from "../json/skils.json";
+
+function SkillsList(list) {
+  const img_path = "../image/product_logo/";
+  const content = list.map((data, index) =>
+    <li key={index}>
+      <div className="skil-img"><img src={img_path + data.img} alt={data.name} /></div>
+      <p className="text-center">{data.name}</p>
+      <small>{data.content}</small>
+    </li>
+  );
+
+  return content;
+}
+
+const SkilsPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <article id="skils">
+      <h2>SKILS</h2>
+      <section id="front">
+        <div>
+          <h3>FRONTEND DEVELOPMENT</h3>
+          <ul>{SkillsList(skilsFile.frontendDevelopment)}</ul>
+        </div>
+      </section>
+      <section id="back">
+        <div>
+          <h3>BACKEND DEVELOPMENT</h3>
+          <ul>{SkillsList(skilsFile.backendDevelopment)}</ul>
+        </div>
+      </section>
+      <section id="ops">
+        <div>
+          <h3>DEVELOPMENT OPERATIONS</h3>
+          <ul>{SkillsList(skilsFile.developmentOperations)}</ul>
+        </div>
+      </section>
+    </article>
+
+    <article id="works">
+      <h2>WORKS</h2>
+      <p className="text-center comming-soon">Comming Soon...</p>
+    </article>
+  </Layout>
+)
+
+export default SkilsPage
