01067ccc5b0ef3d26cedbbcdc31950a0785da59c
Mell <merciapp217@icloud.com>
2019-11-14 01:30:46 +0900

    header整える

--- src/components/header.js
+++ src/components/header.js
@@ -2,21 +2,19 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const activeLinkStyle = {
-  color: '#d2b356',
-}
-
 const Header = ({ siteTitle }) => (
   <header>
-    <h1><Link to="/">{siteTitle}</Link></h1>
-    <nav>
-      <ul>
-        <li><Link to="/profile/" activeStyle={activeLinkStyle}>Profile</Link></li>
-        <li><Link to="/skils/" activeStyle={activeLinkStyle}>Skils</Link></li>
-        <li><Link to="/contact/" activeStyle={activeLinkStyle}>Contact</Link></li>
-        <li><Link to="/">Blog</Link></li>
-      </ul>
-    </nav>
+    <div className="header">
+      <h1><Link to="/">{siteTitle}</Link></h1>
+      <nav>
+        <ul>
+          <li><Link to="/profile/" activeClassName="active">Profile</Link></li>
+          <li><Link to="/skils/" activeClassName="active">Skils</Link></li>
+          <li><Link to="/contact/" activeClassName="active">Contact</Link></li>
+          <li><Link to="/">Blog</Link></li>
+        </ul>
+      </nav>
+    </div>
   </header>
 )
 
